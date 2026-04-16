af9e92c85360e9c3be14a49ef82b1519aa12b240
haripriyavenkatachalam04 <haripriyavenkatachalam04@gmail.com>
2026-04-16 22:26:24 +0530

    Set BrowserRouter basename to '/agriconnectai'

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/agriconnectai">
         <AppLayout>
           <Routes>
             <Route path="/" element={<Index />} />
